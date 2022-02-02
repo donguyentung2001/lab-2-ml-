d11aa1ce846dc7ebf12aed3d5834bc006cbd03e0
donguyentung2001 <54921286+donguyentung2001@users.noreply.github.com>
2022-02-02 12:09:54 -0500

    Update train.py

--- train.py
+++ train.py
@@ -25,6 +25,7 @@ import matplotlib.pyplot as plt
 mnist = fetch_openml('mnist_784', version=1, as_frame=False)
 X, y = mnist["data"], mnist["target"]
 
+X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
 
 # To plot pretty figures
 
